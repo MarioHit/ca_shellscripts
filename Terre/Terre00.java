abd18061900bb12117d316016bfd0c0c946178b0
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-02 09:03:21 +0100

    fin de terre  00

--- Terre/Terre00.java
+++ Terre/Terre00.java
@@ -1,6 +1,16 @@
+/*
+créer un programee qui affiche l'alpabet en lettres 
+miniscules suivi d'un retour à la ligne.
 
+Attention votre programme devra utiliser une boucle  !! 
+*/
 public class Terre00 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		String alphabet = "abcdefghijklmnopqrsuvwy";
+		String[] tab = alphabet.split("");
+		for (String i: tab) {
+			System.out.print(i);
+		}
+		System.out.print("\n");
 	}
 }
---
+++ Terre/git_p.sh
@@ -0,0 +1,19 @@
+git status;
+echo  ----------1 status
+git add --all ;
+echo -----------2 add
+git status;
+echo -----------3 status 
+echo message de commit svp ? 
+read message
+echo  ceci est le message " $message "
+echo ----------4 message
+git commit -m " $message ";
+echo ----------5 commit
+git status;
+echo ----------6 status 
+git push;
+echo ----------7 push
+
+git status;
+echo ----------8 status
