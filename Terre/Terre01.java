2e97383dd03694aff124a3f2a9d6b9be5b7cdc08
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-02 09:56:46 +0100

    init Terre01.java

---
+++ Terre/Terre01.java
@@ -0,0 +1,13 @@
+/*
+
+créer un programme qui affiche son nom de fichier 
+
+
+*/
+
+public class Terre01 {
+	public static void main(String[] args) {
+		String nomFichier = "Terre01.java";
+		System.out.println(nomFichier);
+	}
+}
