cc567b45db6c2a4f21ee1c60d7d86286cbb3dc0a
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-03 08:48:01 +0100

    init nom de fichier

--- Terre/Terre01.java
+++ Terre/Terre01.java
@@ -8,6 +8,22 @@ créer un programme qui affiche son nom de fichier
 public class Terre01 {
 	public static void main(String[] args) {
 		String nomFichier = "Terre01.java";
-		System.out.println(nomFichier);
+		System.out.println("00-name-"+Terre01.class.getName());
+		System.out.println("00-type name -"+Terre01.class.getTypeName());
+		afficher();
+
+	}
+	
+	void afficher() {
+		System.out.println("01-canonical -"+Terre01.class.getCanonicalName());
+/*
+		String className = this.getClass.getSimpleName();
+		System.out.println("1. class: "+className);
+		String className2 = this.getClass.getSuperClass.getName();
+		System.out.println("2. class: "+className2);
+
+		String canonicalClassName = this.getClass.getCanonicalName();
+		System.out.println("2. canonical : "+canonicalClassName);
+*/
 	}
 }
