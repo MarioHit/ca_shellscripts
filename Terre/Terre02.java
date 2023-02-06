fda3b4fde0f1d79ac37f8d5bfffb9b907460f7d2
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-06 08:05:05 +0100

    Terre02 : afficheur d'arguments ok

---
+++ Terre/Terre02.java
@@ -0,0 +1,16 @@
+
+/*
+
+Afficheur d'arguments 
+
+il faut saisir les arguments en entrée voir ce que ça donne 
+
+*/
+
+public class Terre02 {
+	public static void main(String[] args) {
+		for (String i : args) {
+			System.out.println(i);
+		}
+	}
+}
