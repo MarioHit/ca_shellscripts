eb43d301ece151a07147622ffa212d22c3d8f711
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-11 12:54:53 +0100

    Terre05 modif avec double

--- Terre/Terre05.java
+++ Terre/Terre05.java
@@ -8,21 +8,30 @@ entre deux nombre
 public class Terre05 {
 	public static void main(String[] args) {
 		// récupération des arguments 
-		int arg1 = 1;
-		int arg2 = 1;
-		if ( args.length > 0) {
-			if( args[0] != null && args[0].matches("[0-9]")) {
+		int arg1 = 0;
+		int arg2 = 0;
+		String intRegEx =  "^-?\\+?[0-9]+(.?[0-9]+)?";
+		if ( args.length == 2) {
+			if( args[0] != null && args[0].matches(intRegEx)) {
 				arg1 = Integer.valueOf(args[0]);
 			}
-			if( args[1] != null && args[1].matches("[0-9]")) {
+			if( args[1] != null && args[1].matches(intRegEx)) {
 				arg2 = Integer.valueOf(args[1]);
 			}
-			System.out.println(arg1+" div par "+arg2);
-			System.out.println("Resulat: "+arg1/arg2);
+			if ( arg1 != 0 && arg2 != 0) {
+				System.out.println(arg1+" div par "+arg2);
+				int result = arg1/arg2;
+				int reste = arg2 - result;
+				System.out.println("Resulat: "+result);
+				System.out.println("Reste: "+reste);
+			}else {
+				System.out.println(arg1+" div par "+arg2);
+				System.out.println("Erreur");
+			}
 
 
 		} else {
-			System.out.println("Veullez mettre un argument!");
+			System.out.println("Veullez mettre deux arguments!");
 		}
 	}
 }
