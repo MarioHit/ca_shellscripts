4ba2988d237e3794fb58db1fff4ff52a7f45cd81
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-11 13:01:58 +0100

    Terre05 reste avec modulo

--- Terre/Terre05.java
+++ Terre/Terre05.java
@@ -10,7 +10,7 @@ public class Terre05 {
 		// récupération des arguments 
 		int arg1 = 0;
 		int arg2 = 0;
-		String intRegEx =  "^-?\\+?[0-9]+(.?[0-9]+)?";
+		String intRegEx =  "^-?\\+?[0-9]+";
 		if ( args.length == 2) {
 			if( args[0] != null && args[0].matches(intRegEx)) {
 				arg1 = Integer.valueOf(args[0]);
@@ -21,7 +21,7 @@ public class Terre05 {
 			if ( arg1 != 0 && arg2 != 0) {
 				System.out.println(arg1+" div par "+arg2);
 				int result = arg1/arg2;
-				int reste = arg2 - result;
+				int reste = arg1 % arg2;
 				System.out.println("Resulat: "+result);
 				System.out.println("Reste: "+reste);
 			}else {
