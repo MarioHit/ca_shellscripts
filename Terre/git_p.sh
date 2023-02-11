4739d32825344c0c2840482c032f81dbf21f7a42
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-11 13:13:03 +0100

    modif fichier git

--- Terre/git_p.sh
+++ Terre/git_p.sh
@@ -1,21 +1,19 @@
 git status;
-echo  ----------1 status
+echo  ----------0 status
+git diff;
+echo -----------1 diff 
 git add --all ;
 echo -----------2 add
 git status;
 echo -----------3 status 
 echo message de commit svp ? 
 read message
-echo  ceci est le message " $message " 
-git diff;
-echo ----------3.1 diff
+echo  ceci est le message " $message "
 echo ----------4 message
 git commit -m " $message ";
 echo ----------5 commit
 git status;
 echo ----------6 status 
-git diff;
-echo ----------6.1 diff 
 
 git push;
 echo ----------7 push
