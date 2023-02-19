c1211a02522470b51b6a29330d27a2e374d8da33
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-19 23:17:49 +0100

    Terre09 Racine carre boucle centaine miillier

--- Terre/Terre09.java
+++ Terre/Terre09.java
@@ -28,20 +28,21 @@ public class Terre09 {
                             //System.out.println(i+" > "+ x+"² = "+x*x+ " ~"+racineProche+" -->"+racineProche*racineProche);
                             //System.out.println("---->racine de "+ i +" = " + Math.sqrt(carre));
                         }
-                        System.out.println("- "+x*x);
+                        // System.out.println("- "+x*x);
                         if (x * x < carre ) {
                             racineP = x;
-                            // System.out.println("haha x > "+x);
+                            //System.out.println("- "+i+"  -haha x > "+x);
                         }
                     }
-                    System.out.println(" Bas "+ racineProche +" -- GGOOD : "+ racineP);
                     if( racine == 0 ) {
                        //  System.out.println("~~ racine de "+ i +" ~ " + racineProche +" pour "+racineProche*racineProche +" ou " + (racineProche+1) +" pour "+(racineProche+1)*(racineProche+1));
                         // System.out.println("---->racine de "+ i +" = " + Math.sqrt(carre));
                         double in = 0;
+                        double inm = 0;
                         double in2 = 0;
                         double racineD2 = 0;
-                        double rac = 0;
+                        double racCentaine = 0;
+                        double racMil = 0;
                         double racinePrD = Double.valueOf(racineProche) ;
                         double carreD = Double.valueOf(i);
                         double diff = Double.MAX_VALUE;
@@ -51,13 +52,28 @@ public class Terre09 {
                             racineD2 = racineD*racineD;
                             double difference = carreD - racineD2;
                             if ( racineD2 < carreD) {
-                                rac = racineD;
+                                racCentaine = racineD;
                             }
-                            System.out.println(in2+" __ "+in+" - "+racineD +"² : "+ racineD2 +" ~ "+difference + " ~ "+carreD);
+                            //System.out.println(in2+" __ "+in+" - "+racineD +"² : "+ racineD2 +" ~ "+difference + " ~ "+carreD);
                             in += 0.1;
                             in2++;
                         }
-                        System.out.println("---->racine de "+ i +" = " + Math.sqrt(carre) +" : "+rac+"  pour à peu pres : "+rac*rac);
+                        System.out.println("---->racine de "+ i +" = " + Math.sqrt(carre) +" : "+racCentaine+"  pour à peu pres : "+racCentaine*racCentaine);
+                        double carreCentaine = racCentaine * racCentaine;
+                        while ( carreCentaine < carreD) {
+                            double x = inm / 10.0;
+                            double racineD = racCentaine + x;
+                            carreCentaine = racineD*racineD;
+                            double difference = carreD - racineD2;
+                            if ( carreCentaine < carreD) {
+                                racMil = racineD;
+                            }
+                            //System.out.println("**"+in2+" __ "+inm+" - "+racineD +"² : "+ racineD2 +" ~ "+difference + " ~ "+carreD);
+                            inm += 0.001;
+                            in2++;
+                        }
+                        System.out.println("---->racine de "+ i +" = " + Math.sqrt(carre) +" : "+racMil+"  pour à peu pres : "+racMil*racMil);
+
                     }
                     //System.out.println("---->racine de "+ i +" = " + Math.sqrt(carre) +" * :"+racine);
                 } else {
