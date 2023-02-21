f0d1352cd098f39d25000293b5d07bd97f82e872
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-21 12:57:24 +0100

    Terre11 24 to 12 ok OPT

--- Terre/Terre11.java
+++ Terre/Terre11.java
@@ -4,26 +4,13 @@ public class Terre11 {
             // vérifier le format
             if( isFormatDate(s)) {
                 String[] data = s.split(":");
-                int hh , hhCalc, hhFinal = 0;
-                hh = Integer.parseInt(data[0]);
-                //hhCalc = Integer.parseInt(data[1]);
-                String hhStr , mmStr , str= "";
-                boolean isPm = hh >= 12 ? true : false;
-                if ( isPm ) {
-                    hhCalc = hh - 12;
-                } else {
-                    hhCalc = hh;
-                }
-                str = isPm? "PM":"AM";
-                System.out.println(" heure : "+hhCalc);
-                if(hhCalc == 0) {
-                    hhFinal = 12;
-                } else {
-                    hhFinal = hhCalc;
-                }
-
-
-                String res = hhFinal+":"+data[1]+ str;
+                int heures , heureSortie = 0;
+                heures = Integer.parseInt(data[0]);
+                boolean isPm = heures >= 12 ? true : false;
+                String str = isPm? "PM":"AM";
+                heures = isPm? heures -12 : heures;
+                heureSortie = heures == 0 ? 12 : heures;
+                String res = heureSortie+":"+data[1]+ str;
                 System.out.println(s+" --> "+res);
             } else {
                 System.out.println("Erreur : '"+s + "' n'est pas une date");
@@ -33,9 +20,5 @@ public class Terre11 {
 
     public static boolean isFormatDate(String str) {
         return str != null && str.matches("[0-9][0-9]:[0-9][0-9]");
-    }
-
-    public static boolean isNum(String str) {
-        return str != null && str.matches("[0-9][0-9]");
     }
 }
