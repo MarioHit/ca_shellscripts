5ce9825a9c5b01e0cd1171191ebbc5ff925b5834
mariohit <gilbertmario.hitimana@gmail.com>
2023-02-24 11:14:41 +0100

    Terre15 fin de l'épreuve OK OK

---
+++ Terre/Terre15.java
@@ -0,0 +1,15 @@
+/**
+ * J'ai terminé Terre 1
+ */
+
+public class Terre15 {
+
+    private static String sentiment  = "Challengeant de faire les fonction" +
+            " sans utiliser les liste et stream" +
+            " je ferai un partie avec les streams par la suite" +
+            " pour juste travailler l'aspect stream vu que l'algo" +
+            " est déjà prêt pour chaque solution";
+    public static void main(String[] args) {
+        System.out.println("J'ai terminé l'Epreuve de la terre , c'étiat "+sentiment);
+    }
+}
